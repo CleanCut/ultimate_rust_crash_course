f4217f684fbad0152d8c8f43971be9c3efbafc5b
Nathan Stocks <cleancut@github.com>
2020-03-21 13:29:10 -0600

    Improve the documentation for the final project

--- exercise/z-final-project/src/main.rs
+++ exercise/z-final-project/src/main.rs
@@ -1,13 +1,30 @@
 // FINAL PROJECT
 //
 // Create an image processing application.  Exactly what it does and how it does
-// it is up to you.  Look for comments labeled **OPTION** below.
+// it is up to you, though I've stubbed a good amount of suggestions for you.
+// Look for comments labeled **OPTION** below.
 //
-// You should be able to complete at least 2 or 3 **OPTION** sections during the
-// time provided for this course.  Feel free to keep going and complete them
-// all, if you like!  Have fun and play around with the code.
+// Two image files are included in the project root for your convenience: dyson.png and pens.png
+// Feel free to use them or provide (or generate) your own images.
+//
+// Don't forget to have fun and play around with the code!
 //
 // Documentation for the image library is here: https://docs.rs/image/0.21.0/image/
+//
+// NOTE 1: Image processing is very CPU-intensive.  Your program will run *noticeably* faster if you
+// run it with the `--release` flag.
+//
+//     cargo run --release [ARG1 [ARG2]]
+//
+// For example:
+//
+//     cargo run --release blur image.png blurred.png
+//
+// NOTE 2: This is how you parse a number from a string (or crash with a
+// message). It works with any integer or float type.
+//
+//     let positive_number: u32 = some_string.parse().expect("Failed to parse a number");
+
 
 fn main() {
     // 1. First, you need to implement some basic command-line argument handling
@@ -16,8 +33,6 @@ fn main() {
     //
     // Challenge: If you're feeling really ambitious, you could delete this code
     // and use the "clap" library instead: https://docs.rs/clap/2.32.0/clap/
-    //
-    //
     let mut args: Vec<String> = std::env::args().skip(1).collect();
     if args.is_empty() {
         print_usage_and_exit();
@@ -51,10 +66,7 @@ fn main() {
         // **OPTION**
         // Grayscale -- see the grayscale() function below
 
-        // **OPTION**
-        // Generate -- see the generate() function below
-
-        // ANOTHER EXAMPLE...a really fun one. :-)
+        // A VERY DIFFERENT EXAMPLE...a really fun one. :-)
         "fractal" => {
             if args.len() != 1 {
                 print_usage_and_exit();
@@ -63,28 +75,15 @@ fn main() {
             fractal(outfile);
         }
 
+        // **OPTION**
+        // Generate -- see the generate() function below -- this should be sort of like "fractal()"!
+
         // For everything else...
         _ => {
             print_usage_and_exit();
         },
     }
 }
-
-// NOTE 1: Image processing is very CPU-intensive.  Your program will run
-// *noticeably* faster if you run it with the --release flag.
-//
-//   cargo run --release [ARG1 [ARG2]]
-//
-// For example:
-//
-//   cargo run --release blur image.png blurred.png
-
-
-// NOTE 2: This is how you parse a number from a string (or crash with a
-// message). It works with any integer or float type.
-//
-// let positive_number: u32 = some_string.parse().expect("Failed to parse a number");
-
 
 fn print_usage_and_exit() {
     println!("USAGE (when in doubt, use a .png extension on your filenames)");
