e36afd7eb08ce37c09b36f15e3e3e81fae48f0f8
Nathan Stocks <cleancut@github.com>
2020-03-21 12:33:18 -0600

    Improve instructions on exercise c

--- exercise/c-simple-types/src/main.rs
+++ exercise/c-simple-types/src/main.rs
@@ -3,38 +3,46 @@
 
 fn main() {
     let coords: (f32, f32) = (6.3, 15.0);
-    // 1. Use print_difference() to show the difference between the two numbers
-    // in coords.  Use tuple indexing. Hint: It's NOT square brackets.
+    // 1. Pass parts of `coords` to the `print_difference` function. This should show the difference
+    // between the two numbers in coords when you do `cargo run`.  Use tuple indexing.
     //
-    // print_difference( ... );
+    // The `print_difference` is defined below the `main` function. It may help if you look at how
+    // it is defined.
+    //
+    //print_difference( ... );   // Uncomment and finish this line
 
 
-    // 2. We want to use print_array() to print coords...but coords isn't an array!
-    //    Create an array of type [f32; 2] and initialize it to contain the
-    //    information from coords.  Uncomment the print_array line and run the code.
+    // 2. We want to use the `print_array` function to print coords...but coords isn't an array!
+    // Create an array of type [f32; 2] and initialize it to contain the
+    // information from coords.  Uncomment the print_array line and run the code.
     //
-    // let coords_arr...               // create the array here
-    // print_array(coords_arr);
+    //let coords_arr...               // create an array literal out of parts of `coord` here
+    //print_array(coords_arr);        // and pass it in here (this line doesn't need to change)
 
 
     let series = [1, 1, 2, 3, 5, 8, 13];
-    // 3. Make the ding() function happy by passing it the value 13 out of the
-    // series array.  Use array indexing.
+    // 3. Make the `ding` function happy by passing it the value 13 out of the `series` array.
+    // Use array indexing.  Done correctly, `cargo run` will produce the additional output
+    // "Ding, you found 13!"
     //
-    // ding(...);
+    //ding(...);
 
 
     let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
-    // 4. Pass the on_off function the value "true" from mess.  I'll get you
-    //    started:
+    // 4. Pass the `on_off` function the value `true` from the variable `mess`.  Done correctly,
+    // `cargo run` will produce the additional output "Lights are on!" I'll get you started:
     //
-    // on_off(mess.2
+    //on_off(mess.2
 
     // 5.  What a mess -- functions in a binary! Let's get organized!
     //
-    //   - Make a library file
-    //   - Move all the functions (except main) into the library
-    //   - Bring all the functions into scope using use statements
+    // - Make a library file (src/lib.rs)
+    // - Move all the functions (except main) into the library
+    // - Make all the functions public with `pub`
+    // - Bring all the functions into scope using use statements. Remember, the name of the library
+    //   is defined in Cargo.toml.  You'll need to know that to `use` it.
+    //
+    // `cargo run` should produce the same output, only now the code is more organized. 🎉
 
     // Challenge: Uncomment the line below, run the code, and examine the
     // output. Then go refactor the print_distance() function according to the
