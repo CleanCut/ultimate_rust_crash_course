320bd7c38aff2e6fd8ed3978638e8678be0fbdd3
Nathan Stocks <cleancut@github.com>
2020-03-21 13:12:38 -0600

    Improve instructions for exercise e

--- exercise/e-ownership-references/src/main.rs
+++ exercise/e-ownership-references/src/main.rs
@@ -13,24 +13,24 @@ fn main() {
             std::process::exit(-1);
         }).to_owned();
 
-    // 1. Write a function "inspect()" that takes a reference to arg and prints
-    //    whether arg is plural or singular.  Then uncomment and run this code.
-    //    Hint: use arg.ends_with("s")
+    // 1. Write a function `inspect` that takes a reference to a String, returns nothing, but
+    // prints whether the contents of the String is plural or singular. Then uncomment and run this
+    // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
+    // String reference
     //
     //inspect(&arg);
 
-    // 2. Write a function "change()" that takes a mutable reference to arg
-    //    and adds an "s" to the string if it doesn't already end with "s".
-    //    Then uncomment and run this code.  Hint: use arg.push_str("s")
+    // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
+    // the String if it doesn't already end with "s". Then uncomment and run the code below with
+    // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
     //
     //change(&mut arg);
     //println!("I have many {}", arg);
 
-    // 3. Write a function "eat" that consumes arg and returns a bool
-    //    indicating whether or not arg both starts with a "b" AND contains an "a".
-    //    Then uncomment and run this code.
-    //    Hint 1: use arg.starts_with("b") and arg.contains("a")
-    //    Hint 2: && is the boolean "AND" operator
+    // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
+    // indicating whether or not the String both starts with a "b" AND contains an "a".
+    // Hint 1: use `.starts_with("b")` and `.contains("a")`
+    // Hint 2: `&&` is the boolean "AND" operator
     //
     //if eat(arg) {
     //    println!("Might be bananas");
@@ -38,10 +38,10 @@ fn main() {
     //    println!("Not bananas");
     //}
 
-    // Try running this program with "boat", "banana", and "grapes"  :-)
+    // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
 
-    // Challenge: Write a function "add" that takes references to two integer
-    // arguments, dereferences them and adds them together, and returns the result.
+    // Challenge: Write a function "add" that takes *references* to two integer arguments,
+    // dereferences them and adds them together, and returns the result.
     //
     // println!("1 + 2 = {}, even via references", add(&1, &2));
 }
