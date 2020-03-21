818ead245ac370c5b1a48e4ff6d8489200c0af4f
Nathan Stocks <cleancut@github.com>
2020-03-21 12:57:51 -0600

    Improve instructions for exercise d

--- exercise/d-control-flow-strings/src/main.rs
+++ exercise/d-control-flow-strings/src/main.rs
@@ -2,23 +2,38 @@
 #![allow(dead_code, unused_mut, unused_variables)]
 
 fn main() {
+    // This collects any command-line arguments into a vector of Strings.
+    // For example:
+    //
+    //     cargo run apple banana
+    //
+    // ...produces the equivalent of
+    //
+    //     vec!["apple".to_string(), "banana.to_string()"]
     let args: Vec<String> = std::env::args().skip(1).collect();
-    for arg in args {
-        // 1. Handle the command-line arguments!
-        //    If arg is "sum", then call the sum() function
-        //    If arg is "double", then call the double() function
-        //    If arg is anything else, then call the count() function, passing "arg" to it.
-        //
-        // Try passing "sum", "double" and something else to the program by adding your argument
-        // after "cargo run".  For example "cargo run sum"
 
+    // This consumes the `args` vector to iterate through each borrowed string slice
+    for arg in args {
+        // 1a. Your task: handle the command-line arguments!
+        //
+        // - If arg is "sum", then call the sum() function
+        // - If arg is "double", then call the double() function
+        // - If arg is anything else, then call the count() function, passing "arg" to it.
+        //
+        // Hint: You will need to convert your string literals into a String before you will be able
+        // to compare them with `==`
+
+
+        // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
+        // after "cargo run".  For example "cargo run sum"
     }
 }
 
 fn sum() {
     let mut sum = 0;
-    // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive*
-    // and add them all together (find the sum).  Hint: You should get 255
+    // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
+    // and add them all together (increment the `sum` variable).  Hint: You should get 255
+    // Run it with `cargo run sum`
 
 
     println!("The sum is {}", sum);
@@ -27,18 +42,19 @@ fn sum() {
 fn double() {
     let mut count = 0;
     let mut x = 1;
-    // 3. Use a "while loop" to count how many times you can double the value of
-    // x (multiply x by 2) before it is larger than 500.  Hint: (it is 9 times)
+    // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
+    // by 2) before it is larger than 500.  Increment `count` each time through the loop. Run it
+    // with `cargo run count`  Hint: The answer is 9 times.
 
 
     println!("You can double x {} times before it is larger than 500", count);
 }
 
 fn count(arg: String) {
-    // Challenge: Use an unconditional loop to print arg 8 times, and then break.
-    // You will need to count your loops, somehow.
+    // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
+    // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break.
+    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
 
 
     println!(); // This will output just a newline at the end for cleanliness.
