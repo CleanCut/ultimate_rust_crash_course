835fde70954ce2d907ebeef3615da13a2bcd8870
jsperafico <jhonny.potter@gmail.com>
2021-04-06 10:51:17 +0000

    module usage

--- exercise/b_functions/src/main.rs
+++ exercise/b_functions/src/main.rs
@@ -9,10 +9,8 @@ fn main() {
     //
     // See if you can fix the error. It is right around here, somewhere.  If you succeed, then
     // doing `cargo run` should succeed and print something out.
-    {
-        let area = area_of(width, height);
-        println!("Area is {}", area);
-    }
+    let area = area_of(width, height);
+    println!("Area is {}", area);
 
     // 2. The area that was calculated is not correct! Go fix the area_of() function below, then run
     //    the code again and make sure it worked (you should get an area of 28).
--- exercise/c_simple_types/src/main.rs
+++ exercise/c_simple_types/src/main.rs
@@ -1,6 +1,8 @@
 // Silence some warnings so they don't distract from the exercise.
 #![allow(dead_code, unused_variables)]
 
+use ding_machine::{print_difference, on_off, print_array, ding, print_distance};
+
 fn main() {
     let coords: (f32, f32) = (6.3, 15.0);
     // 1. Pass parts of `coords` to the `print_difference` function. This should show the difference
@@ -9,15 +11,15 @@ fn main() {
     // The `print_difference` function is defined below the `main` function. It may help if you look
     // at how it is defined.
     //
-    //print_difference( ... );   // Uncomment and finish this line
+    print_difference( coords.0, coords.1 );
 
 
     // 2. We want to use the `print_array` function to print coords...but coords isn't an array!
     // Create an array of type [f32; 2] and initialize it to contain the
     // information from coords.  Uncomment the print_array line and run the code.
     //
-    //let coords_arr...               // create an array literal out of parts of `coord` here
-    //print_array(coords_arr);        // and pass it in here (this line doesn't need to change)
+    let coords_arr: [f32; 2] = [coords.0, coords.1];
+    print_array(coords_arr);
 
 
     let series = [1, 1, 2, 3, 5, 8, 13];
@@ -25,14 +27,14 @@ fn main() {
     // Use array indexing.  Done correctly, `cargo run` will produce the additional output
     // "Ding, you found 13!"
     //
-    //ding(...);
+    ding(series[6]);
 
 
     let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
     // 4. Pass the `on_off` function the value `true` from the variable `mess`.  Done correctly,
     // `cargo run` will produce the additional output "Lights are on!" I'll get you started:
     //
-    //on_off(mess.2
+    on_off(mess.2[1].0);
 
     // 5.  What a mess -- functions in a binary! Let's get organized!
     //
@@ -47,37 +49,5 @@ fn main() {
     // Challenge: Uncomment the line below, run the code, and examine the
     // output. Then go refactor the print_distance() function according to the
     // instructions in the comments inside that function.
-
-    // print_distance(coords);
+    print_distance(coords);
 }
-
-fn print_difference(x: f32, y: f32) {
-    println!("Difference between {} and {} is {}", x, y, (x - y).abs());
-}
-
-fn print_array(a: [f32; 2]) {
-    println!("The coordinates are ({}, {})", a[0], a[1]);
-}
-
-fn ding(x: i32) {
-    if x == 13 {
-        println!("Ding, you found 13!");
-    }
-}
-
-fn on_off(val: bool) {
-    if val {
-        println!("Lights are on!");
-    }
-}
-
-fn print_distance(z: (f32, f32)) {
-    // Using z.0 and z.1 is not nearly as nice as using x and y.  Lucky for
-    // us, Rust supports destructuring function arguments.  Try replacing "z" in
-    // the parameter list above with "(x, y)" and then adjust the a function
-    // body to use x and y.
-    println!(
-        "Distance to the origin is {}",
-        ( z.0.powf(2.0) + z.1.powf(2.0) ).sqrt());
-}
-
