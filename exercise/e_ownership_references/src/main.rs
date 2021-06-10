d6265a35b59d78db40d4498c3185dd76bf54707d
Nathan Stocks <cleancut@github.com>
2021-06-10 14:20:50 -0600

    All changes done live during the first Rust in 3 Weeks session on Jun 10th, 2021

--- exercise/e_ownership_references/src/main.rs
+++ exercise/e_ownership_references/src/main.rs
@@ -14,25 +14,25 @@ fn main() {
     // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
     // String reference
     //
-    //inspect(&arg);
+    inspect(&arg);
 
     // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
     // the String if it doesn't already end with "s". Then uncomment and run the code below with
     // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
     //
-    //change(&mut arg);
-    //println!("I have many {}", arg);
+    change(&mut arg);
+    println!("I have many {}", arg);
 
     // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
     // indicating whether or not the String both starts with a "b" AND contains an "a".
     // Hint 1: use `.starts_with("b")` and `.contains("a")`
     // Hint 2: `&&` is the boolean "AND" operator
     //
-    //if eat(arg) {
-    //    println!("Might be bananas");
-    //} else {
-    //    println!("Not bananas");
-    //}
+    if eat(arg) {
+        println!("Might be bananas");
+    } else {
+        println!("Not bananas");
+    }
 
     // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
 
@@ -40,4 +40,22 @@ fn main() {
     // dereferences them and adds them together, and returns the result.
     //
     // println!("1 + 2 = {}, even via references", add(&1, &2));
+}
+
+fn eat(s: String) -> bool {
+    s.starts_with("b") && s.contains("a")
+}
+
+fn change(s: &mut String) {
+    if !s.ends_with("s") {
+        s.push_str("s");
+    }
+}
+
+fn inspect(s: &String) {
+    if s.ends_with("s") {
+        println!("{} is plural", s);
+    } else {
+        println!("{} is singular", s);
+    }
 }
