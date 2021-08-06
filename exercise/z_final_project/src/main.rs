4cee81d8dd5b288e185757f0c33fbaa3754055c6
Nathan Stocks <cleancut@github.com>
2021-08-06 11:52:43 -0600

    Better style

--- exercise/z_final_project/src/main.rs
+++ exercise/z_final_project/src/main.rs
@@ -25,7 +25,6 @@
 //
 //     let positive_number: u32 = some_string.parse().expect("Failed to parse a number");
 
-
 fn main() {
     // 1. First, you need to implement some basic command-line argument handling
     // so you can make your program do different things.  Here's a little bit
@@ -49,7 +48,7 @@ fn main() {
             // **OPTION**
             // Improve the blur implementation -- see the blur() function below
             blur(infile, outfile);
-        },
+        }
 
         // **OPTION**
         // Brighten -- see the brighten() function below
@@ -81,7 +80,7 @@ fn main() {
         // For everything else...
         _ => {
             print_usage_and_exit();
-        },
+        }
     }
 }
 
@@ -141,7 +140,6 @@ fn rotate(infile: String, outfile: String) {
 
     // See blur() for an example of how to save the image.
 }
-
 
 fn invert(infile: String, outfile: String) {
     // See blur() for an example of how to open an image.
