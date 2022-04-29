bfe036a1ea810af8fd142cc66745394739daf4d2
Oobl84 <ruban.rajendran@gmail.com>
2022-04-29 15:51:18 +0100

    completed exercises a - c

--- exercise/b_functions/src/main.rs
+++ exercise/b_functions/src/main.rs
@@ -9,9 +9,7 @@ fn main() {
     //
     // See if you can fix the error. It is right around here, somewhere.  If you succeed, then
     // doing `cargo run` should succeed and print something out.
-    {
-        let area = area_of(width, height);
-    }
+    let area = area_of(width, height);
     println!("Area is {}", area);
 
     // 2. The area that was calculated is not correct! Go fix the area_of() function below, then run
@@ -25,16 +23,20 @@ fn main() {
     //
     // If you get stuck, remember that this is *very* similar to what `area_of` does.
     //
-    //println!("Volume is {}", volume(width, height, depth));
+    println!("Volume is {}", volume(width, height, depth));
 }
 
 fn area_of(x: i32, y: i32) -> i32 {
     // 2a. Fix this function to correctly compute the area of a rectangle given
     // dimensions x and y by multiplying x and y and returning the result.
     //
-    return 0;
+    x * y
     // Challenge: It isn't idiomatic (the normal way a Rust programmer would do things) to use
     //            `return` on the last line of a function. Change the last line to be a
     //            "tail expression" that returns a value without using `return`.
     //            Hint: `cargo clippy` will warn you about this exact thing.
+}
+
+fn volume(x: i32, y: i32, z: i32) -> i32 {
+    x * y * z
 }
---
+++ exercise/c_simple_types/src/lib.rs
@@ -0,0 +1,29 @@
+pub fn print_difference(x: f32, y: f32) {
+    println!("Difference between {} and {} is {}", x, y, (x - y).abs());
+}
+
+pub fn print_array(a: [f32; 2]) {
+    println!("The coordinates are ({}, {})", a[0], a[1]);
+}
+
+pub fn ding(x: i32) {
+    if x == 13 {
+        println!("Ding, you found 13!");
+    }
+}
+
+pub fn on_off(val: bool) {
+    if val {
+        println!("Lights are on!");
+    }
+}
+
+pub fn print_distance((x, y): (f32, f32)) {
+    // Using z.0 and z.1 is not nearly as nice as using x and y.  Lucky for
+    // us, Rust supports destructuring function arguments.  Try replacing "z" in
+    // the parameter list above with "(x, y)" and then adjust the function
+    // body to use x and y.
+    println!(
+        "Distance to the origin is {}",
+        ( x.powf(2.0) + y.powf(2.0) ).sqrt());
+}
