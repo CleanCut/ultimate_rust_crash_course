e7ffbebc0c9f48fd05068025392035c4a284b70e
Michael Stallings <stall84@gmail.com>
2022-09-08 00:47:33 -0400

    Added section on scope with exs and comments

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -14,4 +14,38 @@ fn main() {
         "Firing {} of my {} missiles (which travel at {} warp)!",
         ready, missiles, WARP_FACTOR,
     );
+
+    // You can destructure variables
+    let (silly, dumb, dooda) = ("LOL", "snakes", "sherpa");
+
+    // Variable Scope
+    // Scoping is block by default, and apparently a lot of the rules are the same to
+    // ES6+ javascript. For example:
+    fn scopeEx() {
+        let x = 99;
+        {
+            let y = 88;
+            println!("{} is less than {}", y, x); // 88 is less than 99
+        }
+        // println!("{} is less than {}", y, x); // ERROR (y is dropped from scope immediately leaving block)
+    }
+
+    // Variable Shadowing
+    // This is likely a Rust concept that looks a lot like what you see in
+    // javascript for instance, but they make a point to call it 'shadowing'.
+    // But it's the way to accomplish reassignment, etc.
+    let d = 10;
+    {
+        let d = 50;
+        // In this block d will be 50
+    }
+    // In the outter scope d will be 10
+
+    let mut doggie = "woof!";
+    let doggie = "bark!";
+    // doggie will now be immutable 'bark!'
+
+    let meme = "More Cowbell!!";
+    let meme = 99;
+    // We just changed the type of the variable with 'shadowing'
 }
