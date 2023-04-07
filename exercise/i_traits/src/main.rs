73db28fed5a8c1b64c16f50f64f622800bc7ed36
Nathan Stocks <cleancut@github.com>
2023-04-07 12:47:19 -0600

    another round of improvements to exercise i

--- exercise/i_traits/src/main.rs
+++ exercise/i_traits/src/main.rs
@@ -10,10 +10,11 @@
 
 // 1b. Implement the `Colorful` trait for the `Hat` struct:
 //
-// - The `colorful` method of the `Colorful` trait should return the following String values:
-//   - "red" if the size is 0 through 5
-//   - "green" - if the size is 6 or 7
-//   - "blue" - for any other size
+// The `colorful` method of the `Colorful` trait implementation for the `Hat` struct should return
+// the following String values:
+// - "red" if the size is 0 through 5
+// - "green" - if the size is 6 or 7
+// - "blue" - for any other size
 //
 // The code should compile once 1b is complete, but there will be no output until 1c is completed.
 
@@ -30,33 +31,37 @@ fn main() {
     // let small_hat = Hat { size: 2 };
     // let medium_hat = Hat { size: 7 };
     // let large_hat = Hat { size: 100 };
-    // describe_three_hats(small_hat, medium_hat, large_hat);
+    // describe_three_hats(&small_hat, &medium_hat, &large_hat);
 
-    // 2. Implement the Colorful trait for the type i32. Then uncomment and run the code below.
-    //
-    // The `colorful` method for an i32 should return the String values:
+    // 2. Implement the Colorful trait for the type i32. The `colorful` method for an i32 should
+    // return these String values:
     //   - "orange" - If the number is even (see hint)
     //   - "purple" - If the number is odd
     //
+    // Then uncomment and run the code below.
+    //
     // Hint: You may want to use the `is_even` function (see the bottom of this file).
 
-    // for i in range(0..=3) {
-    //     println!("{} is {}", i, i.color());
-    // }
+    // println!("4 is {}", 4.color());
+    // println!("5 is {}", 5.color());
 
-    // 3: Write a generic function named `fortune` that takes anything that implements the
+    // 3. Create a trait named `EvenOdd` with a method `is_even` that takes an immutable
+    // reference to self and returns whether or not self is even. Implement EvenOdd for i32. Then
+    // refactor the `colorful` method for `i32` to use the is_even method.
+
+    // Challenge: Write a generic function named `fortune` that takes anything that implements the
     // Colorful trait and prints out the color in some message (for example: "The color I see in
     // your future is ..."). Then uncomment and run the code below.
+    //
+    // Hint: There's a bit of commented-out code below the main function to help you get started.
 
     // fortune(small_hat);
     // fortune(2);
-
-    // Challenge 2: Create a trait named `EvenOdd` with a method `is_even` that takes an immutable
-    // reference to self and returns whether or not self is even. Implement EvenOdd for i32. Then
-    // refactor the `colorful` method for `i32` to use the is_even method.
 }
 
-// A helpful function
+// fn fortune<T: Colorful>(...
+
+// A function used by some provided code.
 fn describe_three_hats(hat1: &Hat, hat2: &Hat, hat3: &Hat) {
     for hat in [hat1, hat2, hat3] {
         let largeness = if hat.size < 3 {
@@ -67,12 +72,10 @@ fn describe_three_hats(hat1: &Hat, hat2: &Hat, hat3: &Hat) {
             "large"
         };
         println!("The {} hat is {}", largeness, hat.color());
-        println!("The {} hat is {}", largeness, hat.color());
-        println!("The {} hat is {}", largeness, hat.color());
     }
 }
 
-// NOTE: A helpful function to check if a number is even.
+// NOTE: You can use this function to check if a number is even (true) or odd (false)
 fn is_even(number: i32) -> bool {
     number % 2 == 0
 }
