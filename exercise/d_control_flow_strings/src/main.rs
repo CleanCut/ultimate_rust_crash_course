f6996fd2d3699b4240a2abdf3966b4102eb2cfcf
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2023-11-24 16:10:36 +0000

    Control flow

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -19,7 +19,13 @@ fn main() {
         // - If arg is "sum", then call the sum() function
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
-
+        if arg.eq("sum") {
+            sum()
+        } else if arg.eq("double") {
+            double()
+        } else { 
+            count(arg) 
+        }
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
@@ -32,6 +38,9 @@ fn sum() {
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
 
+    for num in 7..=23 {
+        sum += num
+    }
 
     println!("The sum is {}", sum);
 }
@@ -42,6 +51,10 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
+    while x < 500 { 
+        x *= 2;
+        count += 1;
+    }
 
 
     println!("You can double x {} times until x is larger than 500", count);
@@ -51,8 +64,12 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
+    let mut count = 0;
+    loop {
+        print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+        count += 1;
+        if count == 8 { break }
+    }
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
