ba6d2b85d823cb5815ab372384288910c65b92e7
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2023-11-24 10:55:33 +0000

    Variables challenge

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+fn main() {
+
+    let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    missiles = missiles - ready;
+    println!("{} missiles left", missiles);
+}
