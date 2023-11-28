bf71a1fa9529dfef962dfc41d090f81608665b80
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2023-11-28 17:28:23 +0000

    Collections Enums

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -10,6 +10,14 @@
 //
 // You will need to complete 1b as well before you will be able to run this program successfully.
 
+use crate::Shot::*;
+
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss,
+}
+
 impl Shot {
     // Here is a method for the `Shot` enum you just defined.
     fn points(self) -> i32 {
@@ -18,6 +26,11 @@ impl Shot {
         // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
         // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
         // - return 0 points if `self` is a Miss
+        match self {
+            Bullseye => { 5 }
+            Hit(x) => if x < 3.0 { 2 } else { 1 }
+            Miss => { 0 }
+        }
     }
 }
 
@@ -34,10 +47,20 @@ fn main() {
     //      - Less than 1.0 -- `Shot::Bullseye`
     //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
     //      - Greater than 5.0 -- `Shot::Miss`
-
+    for coord in arrow_coords {
+        coord.print_description();
+        let distance = coord.distance_from_center();
+        let shot: Shot = if distance <= 1.0 { Bullseye } 
+            else if distance > 1.0 && distance <= 5.0 { Hit(distance) } 
+            else { Miss };
+        shots.push(shot)
+    }
 
     let mut total = 0;
     // 3. Finally, loop through each shot in shots and add its points to total
+    for shot in shots {
+        total += shot.points()
+    }
 
     println!("Final point total is: {}", total);
 }
