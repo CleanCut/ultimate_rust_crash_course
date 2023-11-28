4a80a0fe4ebd533d93c5d3f23610b72b3c2b9ceb
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2023-11-28 10:16:40 +0000

    Structs traits

--- exercise/f_structs_traits/src/main.rs
+++ exercise/f_structs_traits/src/main.rs
@@ -4,21 +4,29 @@
 // want to bite something.  Once this trait is defined, you should be able to run the program with
 // `cargo run` without any errors.
 //
-//  trait Bite...
+trait Bite {
+    fn bite(&mut self);
+}
 
 
 // 2. Now create a struct named Grapes with a field that tracks how many grapes are left.  If you
 // need a hint, look at how it was done for Carrot at the bottom of this file (you should probably
 // use a different field, though).
 //
-// #[derive(Debug)] // include this line right before your struct definition
-// struct Grapes...
+#[derive(Debug)] // include this line right before your struct definition
+struct Grapes {
+    amount_left: u8,
+}
 
 
 // 3. Implement Bite for Grapes.  When you bite a Grapes, subtract 1 from how many grapes are left.
 // If you need a hint, look at how it was done for Carrot at the bottom of this file.
 //
-// impl Bite for...
+impl Bite for Grapes {
+    fn bite(&mut self) {
+        self.amount_left -= 1;
+    }
+}
 
 
 fn main() {
@@ -30,9 +38,9 @@ fn main() {
     // 4. Uncomment and adjust the code below to match how you defined your
     // Grapes struct.
     //
-    //let mut grapes = Grapes { amount_left: 100 };
-    //grapes.bite();
-    //println!("Eat a grape: {:?}", grapes);
+    let mut grapes = Grapes { amount_left: 100 };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
 
     // Challenge: Uncomment the code below. Create a generic `bunny_nibbles`
     // function that:
@@ -41,8 +49,14 @@ fn main() {
     // Hint: Define the generic type between the function name and open paren:
     //       fn function_name<T: Bite>(...)
     //
-    //bunny_nibbles(&mut carrot);
-    //println!("Bunny nibbles for awhile: {:?}", carrot);
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
+}
+
+fn bunny_nibbles<T: Bite>(bite_obj: &mut T) {
+    for _ in 1..=3 {
+        bite_obj.bite()    
+    }
 }
 
 #[derive(Debug)] // This enables using the debugging format string "{:?}"
@@ -51,7 +65,7 @@ struct Carrot {
 }
 
 impl Bite for Carrot {
-    fn bite(self: &mut Self) {
+    fn bite(&mut self) {
         // Eat 20% of the remaining carrot. It may take awhile to eat it all...
         self.percent_left *= 0.8;
     }
