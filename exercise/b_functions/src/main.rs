af2e7c821274b1595ff19ed208387a10595fea11
Jongwon Kang <krnomad@gmail.com>
2024-04-27 07:19:42 +0900

    complete b excersise

--- exercise/b_functions/src/main.rs
+++ exercise/b_functions/src/main.rs
@@ -11,30 +11,23 @@ fn main() {
     // doing `cargo run` should succeed and print something out.
     {
         let area = area_of(width, height);
+        println!("Area is {}", area);
     }
-    println!("Area is {}", area);
 
-    // 2. The area that was calculated is not correct! Go fix the area_of() function below, then run
-    //    the code again and make sure it worked (you should get an area of 28).
+    // 2. 계산된 영역이 올바르지 않습니다! 아래의 area_of() 함수를 수정한 다음 코드를 다시 실행하고 올바르게 작동하는지 확인하세요 (영역은 28이어야 합니다).
 
-    // 3. Uncomment the line below.  It doesn't work yet because the `volume` function doesn't exist.
-    //    Create the `volume` function!  It should:
-    //    - Take three arguments of type i32
-    //    - Multiply the three arguments together
-    //    - Return the result (which should be 280 when you run the program).
+    // 3. 아래의 줄 주석을 해제하세요. `volume` 함수가 아직 작동하지 않습니다.
+    //    `volume` 함수를 생성하세요! 다음을 따라야 합니다:
+    //    - i32 타입의 세 개의 인수를 가져야 합니다.
+    //    - 세 인수를 곱해야 합니다.
+    //    - 결과를 반환해야 합니다 (프로그램을 실행할 때 결과는 280이어야 합니다).
     //
-    // If you get stuck, remember that this is *very* similar to what `area_of` does.
+    // 곤란할 경우, 이것이 `area_of`가 하는 것과 *매우* 유사하다는 것을 기억하세요.
     //
-    //println!("Volume is {}", volume(width, height, depth));
+    //println!("체적은 {}", volume(width, height, depth));
+
 }
 
 fn area_of(x: i32, y: i32) -> i32 {
-    // 2a. Fix this function to correctly compute the area of a rectangle given
-    // dimensions x and y by multiplying x and y and returning the result.
-    //
-    return 0;
-    // Challenge: It isn't idiomatic (the normal way a Rust programmer would do things) to use
-    //            `return` on the last line of a function. Change the last line to be a
-    //            "tail expression" that returns a value without using `return`.
-    //            Hint: `cargo clippy` will warn you about this exact thing.
+    x*y
 }
