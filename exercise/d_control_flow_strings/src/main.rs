f5f8f234346890475ad8a0afef1835e6130c1abe
Tarane-Konect <tarane.t@konect.ai>
2024-06-18 19:41:39 -0700

    finished step 2 and 3 in part D

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -40,11 +40,13 @@ fn run(){
 
 }
 fn sum() {
-    let mut sum = 0;
+    let mut sum: i32 = 0;
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
-
+        for i in 7..=23{
+            sum+=i;
+        } 
 
     println!("The sum is {}", sum);
 }
@@ -55,7 +57,10 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
-
+    while x<500{
+        x*=2;
+        count+=1;
+    }
 
     println!("You can double x {} times until x is larger than 500", count);
 }
