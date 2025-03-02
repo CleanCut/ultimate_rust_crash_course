b6fdb2022a6d0c8f65183ab772eaee71b4465396
cpmachado <cpmachado@protonmail.com>
2025-03-02 00:11:45 +0000

    1. exercise

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -2,27 +2,16 @@
 #![allow(dead_code, unused_mut, unused_variables)]
 
 fn main() {
-    // This collects any command-line arguments into a vector of Strings.
-    // For example:
-    //
-    //     cargo run apple banana
-    //
-    // ...produces the equivalent of
-    //
-    //     vec!["apple".to_string(), "banana".to_string()]
     let args: Vec<String> = std::env::args().skip(1).collect();
 
-    // This consumes the `args` vector to iterate through each String
     for arg in args {
-        // 1a. Your task: handle the command-line arguments!
-        //
-        // - If arg is "sum", then call the sum() function
-        // - If arg is "double", then call the double() function
-        // - If arg is anything else, then call the count() function, passing "arg" to it.
-
-
-        // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
-        // after "cargo run".  For example "cargo run sum"
+        if arg == "sum" {
+            sum();
+        } else if arg == "double" {
+            double();
+        } else {
+            count(arg);
+        }
     }
 }
 
